dba77e64b880c3abd7d05546caae09616e85c394
daxpatel235 <pateldax23056@gmail.com>
2025-12-22 20:30:00 +0530

    Add files via upload

---
+++ 01_Basics/variables.py
@@ -0,0 +1,21 @@
+#1. this file contains variables and i try to make all details of variables in one file so make sure to read numbers of questions 
+
+student_1 = 90002385
+student_2 = 30000000
+
+avg = (student_1+student_2)/2
+
+print(avg)    # the avg of two students marks is 91.5 
+
+#2. valid variable and invalid variable 
+"""
+the student_1 and student_2 is valid variables,
+but student 1 and student 2 is invallid variable,
+
+in the python language variable only defined as = A-Z, 0-9 and _ only and no space """ 
+
+#3. comments 
+"""
+ single line comments are denoted as #, ex :- #this is code of python
+
+ and multi line comment you just readed , surround the entire block with triple quotes"""
