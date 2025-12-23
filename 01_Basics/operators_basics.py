409890c42b5b14b419eae1885df661f605ee0357
daxpatel235 <pateldax23056@gmail.com>
2025-12-23 20:50:33 +0530

    Add files via upload

---
+++ 01_Basics/operators_basics.py
@@ -0,0 +1,31 @@
+"""
+In Python, operators are special symbols or keywords used to perform operations on values and variables, 
+which are called operands
+"""
+
+
+#1. arithmatic operators 
+
+print(23+56)      #this operator is used for sum
+print(23-56)      #this operator is used for subtraction 
+print(23*56)      #this operator is used for multiply
+print(23/56)      #this operator is used for division 
+print(23//56)     #this operator is used for floor division 
+print(23%56)      #this operator is used for modulo 
+print(3**3)       #this operator is used for power 
+
+#2. assignment operators
+
+a = 1 # so the 1 is assigned to a means that the value of a is 1
+
+b = 2 # so the 2 is assigned to b means that the value of b is 2
+
+"""its like box a and b and we are putting a balls have number like 1 and 2 and its stored the value of balls"""
+
+#3. use assignment operators with arithmatic operators 
+
+a = 5  #sign 5 in a veriable 
+a*=2   # this means 5*2 = 10
+a/=5   # this means 10 
+a+=23
+print(a)
